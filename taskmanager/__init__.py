a624d2bc070da68d16394a84e19114b8432d45dd
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2024-07-07 22:04:23 +0000

    Finally fix applicaiton error by setting app.config[SQLALCHEMY_TRACK_MODIFICATIONS] = False. Create db in terminal

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,14 +1,19 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
+
 if os.path.exists("env.py"):
-	import env #noqa
+    import env  
 
 
-app = Flask(__name__)	
+app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db = SQLAlchemy(app)
 
-from taskmanager import routes #noqa
+from taskmanager import routes
+
