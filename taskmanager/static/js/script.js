e3973333493f22bad5042382a8c2e6fe9f6c0304
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2024-07-08 14:30:14 +0000

    Add functionality to add a task

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,10 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
 
-  
-  document.addEventListener('DOMContentLoaded', function() {
     let confirm_delete = document.querySelectorAll('.modal');
     M.Modal.init(confirm_delete);
+
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmm yyyy",
+      i18n: {done: "Select"},
+    });
+
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+    
   });
