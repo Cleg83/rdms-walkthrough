00a2fd9d10b6a84aee4ff9200287a864c70f0f01
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2024-07-08 13:38:09 +0000

    Add delete button functionality and modal for deleting a category

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,3 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
   });
+
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    let confirm_delete = document.querySelectorAll('.modal');
+    M.Modal.init(confirm_delete);
+  });
